778df8d37af10fbd713de3bb7bc4c731791540e8
Armando <armando.j1221@gmail.com>
2019-07-20 10:46:26 -0500

    Creacion del metodo mostrarMenu

--- AirportShedule/src/main/java/AirportShedule/App.java
+++ AirportShedule/src/main/java/AirportShedule/App.java
@@ -4,8 +4,12 @@ package AirportShedule;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+
+    public void mostrarMenu(){
+        System.out.println("Hola Mundo");
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
